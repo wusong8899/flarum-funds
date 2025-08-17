94ed21149ded502a55b6a4b8b6df24887c59b5b1
wusong8899 <wusong8899@users.noreply.github.com>
2025-08-17 23:08:55 +0900

    f

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,7 +1,7 @@
 import app from 'flarum/admin/app';
 import WithdrawalManagementPage from './components/WithdrawalManagementPage';
 
-app.initializers.add('wusong8899/flarum-withdrawal', () => {
+app.initializers.add('wusong8899-flarum-withdrawal', () => {
   app.extensionData
     .for('wusong8899-flarum-withdrawal')
     .registerPage(WithdrawalManagementPage);
