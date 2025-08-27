8a421fe0bbddf285cc63a3f6a71576e6fd74c267
wusong8899 <wusong8899@users.noreply.github.com>
2025-08-28 07:06:48 +0900

    f

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -10,12 +10,5 @@ app.initializers.add('wusong8899-withdrawal', () => {
 
   app.extensionData
     .for('wusong8899-withdrawal')
-    .registerSetting({
-      setting: 'wusong8899-withdrawal.moneyIconUrl',
-      type: 'url',
-      label: app.translator.trans('withdrawal.admin.settings.money_icon_url'),
-      help: app.translator.trans('withdrawal.admin.settings.money_icon_url_help'),
-      placeholder: 'https://i.mji.rip/2025/08/28/cd18932c68e9bbee9502b1fb6317cba9.png'
-    })
     .registerPage(WithdrawalManagementPage);
 });
