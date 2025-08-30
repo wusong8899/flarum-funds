7b7c9a91d6c729b37c1860c7974b80c8c86a2542
wusong8899 <wusong8899@users.noreply.github.com>
2025-08-30 11:56:45 +0900

    sql f

--- migrations/2024_08_30_000012_add_qr_code_image_url_to_deposit_platforms.php
+++ migrations/2024_08_30_000012_add_qr_code_image_url_to_deposit_platforms.php
@@ -4,5 +4,5 @@ use Flarum\Database\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
 return Migration::addColumns('deposit_platforms', [
-    'qr_code_image_url' => ['string', 'length' => 500, 'nullable' => true, 'after' => 'qr_code_template']
+    'qr_code_image_url' => ['string', 'length' => 500, 'nullable' => true, 'after' => 'icon_class']
 ]);
